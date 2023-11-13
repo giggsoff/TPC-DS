cdf845b3fab592ec4f942f23687c4b5e1b648df5
Ivan Ievlev <ivanievlev@gmail.com>
2023-11-13 13:44:08 +0300

    Update functions.sh
    #LD_PRELOAD=/lib64/libz.so.1 ps is optional and it caused problems on Astra Linux

--- functions.sh
+++ functions.sh
@@ -10,7 +10,8 @@ if [ "$count" -gt "0" ]; then
 	unalias ls
 fi
 
-export LD_PRELOAD=/lib64/libz.so.1 ps
+#LD_PRELOAD=/lib64/libz.so.1 ps is optional and it caused problems on Astra Linux
+#export LD_PRELOAD=/lib64/libz.so.1 ps
 
 LOCAL_PWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OSVERSION=`uname`
